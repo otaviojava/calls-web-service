eda10e8f608f0e6426c62441c4eac3f4426b0f3a
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-02 16:25:41 +0000

    start to create test with period

--- service/src/test/java/my/compary/service/PeriodTest.java
+++ service/src/test/java/my/compary/service/PeriodTest.java
@@ -1,6 +1,18 @@
 package my.compary.service;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class PeriodTest {
-  
+
+    @Test
+    public void shouldReturnNPE() {
+        Assertions.assertThrows(NullPointerException.class, () -> Period.of(null, LocalDateTime.now()));
+        Assertions.assertThrows(NullPointerException.class, () -> Period.of(LocalDateTime.now(), null));
+    }
+
 }
