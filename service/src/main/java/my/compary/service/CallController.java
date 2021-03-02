fa2c2636279176446518b3e91999fa7be67964c8
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-02 18:41:25 +0000

    creates call controller

---
+++ service/src/main/java/my/compary/service/CallController.java
@@ -0,0 +1,12 @@
+package my.compary.service;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/calls")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class CallController {
+}
