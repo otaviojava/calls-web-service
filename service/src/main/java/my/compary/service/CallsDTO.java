ca1a3e7105fcf2ff862d36fc1b92094e2a768546
Otavio Santana <otaviopolianasantana@gmail.com>
2021-03-02 19:45:18 +0000

    creates constructor to callsdto

--- service/src/main/java/my/compary/service/CallsDTO.java
+++ service/src/main/java/my/compary/service/CallsDTO.java
@@ -2,13 +2,21 @@ package my.compary.service;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CallsDTO {
 
     @NotNull(message = "calls may not be blank")
     @Min(value = 1, message = "The list cannot be empty")
-    private List<CallDTO> dtos;
+    private List<CallDTO> dtos = new ArrayList<>();
+
+    public CallsDTO(List<CallDTO> dtos) {
+        this.dtos = dtos;
+    }
+
+    public CallsDTO() {
+    }
 
     public List<CallDTO> getDtos() {
         return dtos;
@@ -16,5 +24,9 @@ public class CallsDTO {
 
     public void setDtos(List<CallDTO> dtos) {
         this.dtos = dtos;
+    }
+
+    void add(CallDTO dto) {
+        this.dtos.add(dto);
     }
 }
